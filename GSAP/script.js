f71cc763a8253fb1b4edcc4d2ed0932eda22a1e8
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-01-28 14:32:43 +0530

    GSAP-P2

---
+++ GSAP/script.js
@@ -0,0 +1,6 @@
+gsap.from("#nav .nav-part1 ,#nav .nav-part2 .c1,#nav .nav-part2 .c2,#nav .nav-part2 .c3,#nav .nav-part3 h5,#nav .nav-part3 button",{
+    y:-100,
+    duration:1,
+    delay:0.5,
+    stagger: 0.1, 
+})

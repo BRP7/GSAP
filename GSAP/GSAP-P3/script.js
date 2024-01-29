b627e187de1cd331a31b88e05ec5012052b65f12
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-01-29 23:51:49 +0530

    MOQO

---
+++ GSAP/GSAP-P3/script.js
@@ -0,0 +1,12 @@
+gsap.to("#page1 h1",{
+    transform:"TranslateX(-100%)",
+    fontWeight:"100",
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"body",
+        markers:true,
+        start:"top 0",
+        end:"top -200%",
+        scrub:2
+    }
+})

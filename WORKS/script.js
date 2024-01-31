418018c8317fe52c2d844595a4d07e48343b4859
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-01-31 15:34:56 +0530

    LocomotiveJS

--- WORKS/script.js
+++ WORKS/script.js
@@ -1,23 +1,34 @@
-var t1=gsap.timeline();
-
-t1.to("#yellow1",{
-    top:"-100%",
-    delay:0.5,
-    duration:1,
-    ease:"expo.out"
-})
-t1.from("#loader #yellow2",{
-    top:"100%",
-    delay:0.5,
-    duration:0.5,
-    ease:"expo.out"
-},"anim")
-t1.to("#loader h1",{
-    color:"black",
-    duration:0.5,
-    delay:0.5
-},"anim")
-t1.to("#loader",{
-    opacity:0,
-
-})
+function loading() {
+    var t1=gsap.timeline();
+    
+    t1.to("#yellow1",{
+        top:"-100%",
+        delay:0.5,
+        duration:1,
+        ease:"expo.out"
+    })
+    t1.from("#loader #yellow2",{
+        top:"100%",
+        delay:0.5,
+        duration:0.5,
+        ease:"expo.out"
+    },"anim")
+    t1.to("#loader h1",{
+        color:"black",
+        duration:0.5,
+        delay:0.5
+    },"anim")
+    t1.to("#loader",{
+        opacity:0,
+    
+    }) 
+    t1.to("#loader",{
+        display:none
+    
+    }) 
+}
+loading();
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});

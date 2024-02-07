9872bb1f51537400014ec6910958e36f24ef982e
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-02-07 14:17:53 +0530

    Cursor Effect Using GSAP

--- Rejouice/script.js
+++ Rejouice/script.js
@@ -1,8 +1,41 @@
-let pageCon =  document.querySelector(".page-content");
+function cursorEffect(){
+    let pageCon =  document.querySelector(".page-content");
 let cursor = document.querySelector(".cursor");
 
 pageCon.addEventListener("mousemove",function(dets){
-    //  console.log(dets.target;X)
-    cursor.style.left = dets.x + "px";
-    cursor.style.top = dets. y+ "px";
+//     //  console.log(dets.target;X)
+//     cursor.style.left = dets.x + "px";
+//     cursor.style.top = dets. y+ "px";
+       gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+       }) 
+})
+pageCon.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+pageCon.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    })   
+})
+
+}
+cursorEffect();
+
+gsap.from(".elem h1",{
+    y:120,
+    stagger:0.2,
+    duration:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        start:"top 47%",
+        end:"top 46%",
+        scrub:2
+    }
 })

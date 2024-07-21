d01b4316f91a8fe429641f0cdea94c3bafd0827f
BRP7 <bhumiparmar0007@gmail.com>
2024-07-21 18:15:33 +0530

    First Horizontal scroll

---
+++ HorizontalScroll/script.js
@@ -0,0 +1,22 @@
+var t1 =gsap.timeline();
+
+t1.from("#nav h2",{
+    y:-20,
+    opacity:0,
+    duration:0.5,
+    delay:0.5
+})
+
+t1.from("#nav #part2 h4",{
+    y:-20,
+    opacity:0,
+    duration:0.5,
+    stagger:1
+})
+
+t1.from("#main h1",{
+    y:-20,
+    opacity:0,
+    duration:0.5,
+    scale:2
+})

c895411a5705e646927f93fefe0a23ec4eec2880
BRP7 <bhumiparmar0007@gmail.com>
2024-07-21 14:33:37 +0530

    gsap basic

---
+++ script.js
@@ -0,0 +1,21 @@
+var t1 = timeline.gsap();
+
+t1.to("#nav h2",{
+    y:30,
+    opacity:1,
+    duration:1,
+    delay:1
+})
+
+t1.to("#nav h4",{
+    y:30,
+    opacity:1,
+    duration:1,
+    stagger:"true"
+})
+
+t1.to("h1",{
+    y:30,
+    opacity:1,
+    duration:1,
+})

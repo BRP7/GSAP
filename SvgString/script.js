40f189f8ab3c1fe21e83693d9d945464d7f1f954
BRP7 <bhumiparmar0007@gmail.com>
2024-07-23 01:24:27 +0530

    String Svg

--- SvgString/script.js
+++ SvgString/script.js
@@ -1,37 +1,62 @@
-var t1 =gsap.timeline();
+var t1 = gsap.timeline();
 
-t1.from("#nav h2",{
-    y:-20,
-    opacity:0,
-    duration:0.5,
-    delay:0.5
+t1.from("#nav h2", {
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5
 })
 
-t1.from("#nav #part2 h4",{
-    y:-20,
-    opacity:0,
-    duration:0.5,
-    stagger:1
+t1.from("#nav #part2 h4", {
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 1
 })
 
-t1.from("#main h1",{
-    y:-20,
-    opacity:0,
-    duration:0.5,
-    scale:2
+t1.from("#main h1", {
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+    scale: 2
 })
 
-gsap.to("#page2 h1",{
-    transform:"translate(-150%)",
-    scrollTrigger:{
-        trigger:"#page2",//tigger must be applied on parent
-        scroller:"body",
-        markers:true,
-        start:"top 0%",
-        end:"top -150%",
-        scrub:2,
-        pin:true
+gsap.to("#page2 h1", {
+    transform: "translate(-150%)",
+    scrollTrigger: {
+        trigger: "#page2",//tigger must be applied on parent
+        scroller: "body",
+        markers: true,
+        start: "top 0%",
+        end: "top -150%",
+        scrub: 2,
+        pin: true
     }
-    
+
 })
 
+var path = `M 10 300 Q 95 300 180 300`;
+var finalPath = `M 10 300 Q 95 300 180 300`;
+
+var string = document.getElementById("string");
+string.addEventListener("mouseenter", function (dets) {
+    path = `M 10 300 Q ${dets.x} ${dets.y} 180 300`;
+    gsap.to("svg path", {
+        attr: {
+            d: path
+        },
+        duration: 0.2,
+        ease: "power3.out",
+
+    })
+})
+string.addEventListener("mouseleave", function () {
+    gsap.to("svg path", {
+        attr: {
+            d: finalPath
+        },
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)",
+
+    })
+})

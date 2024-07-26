82e52efe9c0f3c56180eb94922a8d94447f71a98
BRP7 <bhumiparmar0007@gmail.com>
2024-07-27 02:41:44 +0530

    Cursor Color for different Pages

--- SvgString/script.js
+++ SvgString/script.js
@@ -1,62 +1,34 @@
-var t1 = gsap.timeline();
+var main = document.getElementById("main");
+var cursor = document.getElementById("cursor");
 
-t1.from("#nav h2", {
-    y: -20,
-    opacity: 0,
-    duration: 0.5,
-    delay: 0.5
-})
+// Calculate page boundaries
+var page1 = document.getElementById("page1");
+var page2 = document.getElementById("page2");
+var page3 = document.getElementById("page3");
 
-t1.from("#nav #part2 h4", {
-    y: -20,
-    opacity: 0,
-    duration: 0.5,
-    stagger: 1
-})
+var rectPage1 = page1.getBoundingClientRect();
+var rectPage2 = page2.getBoundingClientRect();
+var rectPage3 = page3.getBoundingClientRect();
 
-t1.from("#main h1", {
-    y: -20,
-    opacity: 0,
-    duration: 0.5,
-    scale: 2
-})
+main.addEventListener("mousemove", function(event) {
+    gsap.to(cursor, {
+        x: event.clientX,
+        y: event.clientY,
+        duration: 1,
+        ease: 'elastic.out(1,0.2)'
+    });
 
-gsap.to("#page2 h1", {
-    transform: "translate(-150%)",
-    scrollTrigger: {
-        trigger: "#page2",//tigger must be applied on parent
-        scroller: "body",
-        markers: true,
-        start: "top 0%",
-        end: "top -150%",
-        scrub: 2,
-        pin: true
+
+    var elementBelowCursor = document.elementFromPoint(event.clientX, event.clientY);
+    
+
+    if (elementBelowCursor.closest('#page1')) {
+        cursor.style.backgroundColor = "#c7b4b4"; 
+    } else if (elementBelowCursor.closest('#page2')) {
+        cursor.style.backgroundColor = "#726d6d";
+    } else if (elementBelowCursor.closest('#page3')) {
+        cursor.style.backgroundColor = "#c7b8b8"; 
+    } else {
+        cursor.style.backgroundColor = "#c7b4b4"; 
     }
-
-})
-
-var path = `M 10 300 Q 95 300 180 300`;
-var finalPath = `M 10 300 Q 95 300 180 300`;
-
-var string = document.getElementById("string");
-string.addEventListener("mouseenter", function (dets) {
-    path = `M 10 300 Q ${dets.x} ${dets.y} 180 300`;
-    gsap.to("svg path", {
-        attr: {
-            d: path
-        },
-        duration: 0.2,
-        ease: "power3.out",
-
-    })
-})
-string.addEventListener("mouseleave", function () {
-    gsap.to("svg path", {
-        attr: {
-            d: finalPath
-        },
-        duration: 1.5,
-        ease: "elastic.out(1,0.2)",
-
-    })
-})
+});
